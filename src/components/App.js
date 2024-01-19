891613bb472ffa98db7ea1b931298ebc81263b41
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2024-01-19 17:48:45 +0300

    prettier_28_lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import {Users} from "./Users";
-import {Todos} from "./Todos";
-import {useDispatch} from "react-redux";
-import {useEffect} from "react";
-import {loadUsers} from "../redux/action";
-import {Route, Routes} from "react-router-dom";
+import { Users } from './Users'
+import { Todos } from './Todos'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { loadUsers } from '../redux/action'
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+function App () {
   const dispatch = useDispatch()
 
-  useEffect(()=>{
-          dispatch(loadUsers())
+  useEffect(() => {
+    dispatch(loadUsers())
   }, [])
 
   return (
     <div className="App">
-        <Routes>
-            <Route path="/:id?" element={
-                <div className="container">
-                    <div>
-                        <Users />
-                    </div>
-                    <div>
-                        <Todos/>
-                    </div>
-                </div>
-            }/>
-        </Routes>
+      <Routes>
+        <Route path="/:id?" element={
+          <div className="container">
+            <div>
+              <Users/>
+            </div>
+            <div>
+              <Todos/>
+            </div>
+          </div>
+        }/>
+      </Routes>
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
