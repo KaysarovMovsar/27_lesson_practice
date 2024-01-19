6cd12b20f59421818f3ea9d8ab234a7d9121c4c4
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2024-01-20 01:54:24 +0300

    29_lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,52 @@
-import { Users } from './Users'
-import { Todos } from './Todos'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { loadUsers } from '../redux/action'
-import { Route, Routes } from 'react-router-dom'
+import { Users } from './Users';
+import { Todos } from './Todos';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { loadUsers } from '../redux/action';
+import { Route, Routes } from 'react-router-dom';
+import { Button } from './Button';
 
-function App () {
-  const dispatch = useDispatch()
+function App() {
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadUsers())
-  }, [])
+    dispatch(loadUsers());
+  }, [dispatch]);
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/:id?" element={
-          <div className="container">
-            <div>
-              <Users/>
+        <Route
+          path="/:id?"
+          element={
+            <div className="container">
+              <div>
+                <Users />
+              </div>
+              <div>
+                <Todos />
+              </div>
             </div>
-            <div>
-              <Todos/>
-            </div>
-          </div>
-        }/>
+          }
+        />
       </Routes>
-
+      <div className="button_container">
+        <Button
+          size="small"
+          onClick={() => console.log("Small button's click")}
+        >
+          Click small button
+        </Button>
+        <Button onClick={() => console.log("Medium button's click")}></Button>
+        <Button
+          size="large"
+          onClick={() => console.log("large button's click")}
+        >
+          Click large button
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/redux/users.js
+++ src/redux/users.js
@@ -1,24 +1,23 @@
 const initialState = {
-    users: [],
-    loading: false
-}
-
-export const usersReducer = (state = initialState, action) => {
-    switch (action.type){
-        case "load/users/start":
-            return {
-                ...state,
-                loading: true
-            }
-        case "load/users/success":
-            return {
-                ...state,
-                loading: false,
-                users: action.payload
-            }
-
-        default:
-            return state
-    }
+  users: [],
+  loading: false
 };
 
+export const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'load/users/start':
+      return {
+        ...state,
+        loading: true
+      };
+    case 'load/users/success':
+      return {
+        ...state,
+        loading: false,
+        users: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
